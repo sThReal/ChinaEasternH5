ce85ddf57ca2128241a003474ccc413512deeec4
Bob Deng <ly5569dhb@sina.cn>
2018-12-23 14:56:07 +0800

    t

---
+++ js/default.js
@@ -0,0 +1,24 @@
+(function(doc, win) {
+  var docEl = doc.documentElement,
+    size = function() {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      console.log('-------', clientWidth, docEl);
+      if (clientWidth >= 750) {
+        docEl.style.fontSize = '100px';
+      } else {
+        docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+      }
+    };
+  if (!doc.addEventListener) return;
+  win.addEventListener('resize', size, false);
+  doc.addEventListener('DOMContentLoaded', size, false);
+})(document, window);
+
+function remToPx(rem) {
+  return parseInt(rem / 750 * document.documentElement.clientWidth * 100);
+}
+
+function pxToRem(px) {
+  return parseFloat(px * 750 / document.documentElement.clientWidth / 100);
+}
